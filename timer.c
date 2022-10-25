1b00575963fb957ff0e3a9ec9591c33e9e2d17d0
3neko22 <enekoamor22@gmail.com>
2022-10-25 13:28:22 +0200

    Timer aldatuta, process and scheduler

--- timer.c
+++ timer.c
@@ -2,18 +2,20 @@
 
 void* timer(void* arg){
     pthread_mutex_lock(&lock);
-    tickP=0;
-    while (tickP< PERIODO_PROCESSGEN || tickS < PERIODO_SCHEDULER){
-        if(tickP< PERIODO_PROCESSGEN){
-            tickP++;
+    while (1){
+        if(tickP >=PERIODO_PROCESSGEN){
+            tickP=0;
             printf("tick Process\n");
         }
-        if(tickS< PERIODO_SCHEDULER){
-            tickS++;
+        if(tickS >=PERIODO_SCHEDULER){
+            tickS=0;
             printf("tick Scheduler\n");
-        }
+        } 
+        tickP++;
+        tickS++;
         done++;
     }
+
     pthread_cond_signal(&cond);
     pthread_cond_wait(&cond2,&lock);
 }
